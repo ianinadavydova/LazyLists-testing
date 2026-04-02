854664d321192918bf50a857acf0dcf6dc2ab92a
Janina Davydova <janina.davydova@jetbrains.com>
2026-04-03 00:10:34 +0200

    Added test for backward scroll

--- composeApp/src/commonTest/kotlin/BasicScrollToItemTest.kt
+++ composeApp/src/commonTest/kotlin/BasicScrollToItemTest.kt
@@ -1,10 +1,6 @@
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.onNodeWithTag
@@ -20,18 +16,19 @@ class BasicScrollToItemTest {
     private val tagOfList = "TagOfList";
 
     @Test
-    fun scrollToItemInTheMiddleTest() = scrollToItemTest(100, 100)
+    fun scrollToItemInTheMiddleTest() {
+        scrollToItemTest(100, 100)
+    }
 
     @Test
-    fun scrollToLastItemTest() = scrollToItemTest(TotalRowsCount, TotalRowsCount - 10)
+    fun scrollToLastItemTest() {
+        scrollToItemTest(TotalRowsCount, TotalRowsCount - 10)
+    }
 
     @OptIn(ExperimentalTestApi::class)
     private fun scrollToItemTest(indexToScroll: Int, expectedFirstVisibleItem: Int) = runComposeUiTest {
         var listState: LazyListState? = null
         var seenLazyIndexes: HashSet<Int>? = null
-        // Declares a mock UI to demonstrate API calls
-        //
-        // Replace with your own declarations to test the code of your project
 
         setContent {
             listState = rememberLazyListState()
@@ -66,5 +63,51 @@ class BasicScrollToItemTest {
         assertEquals(expectedFirstVisibleItem, listState.firstVisibleItemIndex)
         waitForIdle() // we should wait until LaunchedEffect will be applied
         assertEquals(20, seenLazyIndexes.size)
+    }
+
+    @Test
+    @OptIn(ExperimentalTestApi::class)
+    fun scrollBackWardToTopItemTest() = runComposeUiTest {
+            var listState: LazyListState? = null
+            var seenLazyIndexes: HashSet<Int>? = null
+
+            setContent {
+                listState = rememberLazyListState()
+
+                LaunchedEffect(listState) {
+                    seenLazyIndexes = hashSetOf()
+
+                    snapshotFlow {
+                        listState.isScrollInProgress to listState.layoutInfo.visibleItemsInfo.map { it.index }
+                    }.collect { (isScrolling, visibleIndexes) ->
+                        if (!isScrolling) {
+                            visibleIndexes.forEach { index ->
+                                seenLazyIndexes.add(index)
+                            }
+                        }
+                    }
+                }
+
+                createScrollToItemList(listState, tagOfList)
+            }
+
+            // Tests the declared UI with assertions and actions of the Compose Multiplatform testing API
+            onNodeWithTag(tagOfList).assertExists()
+            assertNotNull(listState)
+            assertNotNull(seenLazyIndexes)
+
+            assertEquals(0, listState.firstVisibleItemIndex)
+            assertEquals(10, seenLazyIndexes.size)
+
+            listState.scrollToItem(TotalRowsCount)
+
+            assertEquals(TotalRowsCount - 10, listState.firstVisibleItemIndex)
+            waitForIdle() // we should wait until LaunchedEffect will be applied
+            assertEquals(20, seenLazyIndexes.size)
+
+            listState.scrollToItem(0)
+            assertEquals(0, listState.firstVisibleItemIndex)
+            waitForIdle() // we should wait until LaunchedEffect will be applied
+            assertEquals(20, seenLazyIndexes.size)
     }
 }
